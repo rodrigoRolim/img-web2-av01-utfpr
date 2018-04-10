5644d8a42f5407c3e2f1cc58c51c7f82435b4196
Aluno <Aluno@10.20.39.202>
2018-04-10 19:42:54 -0300

    inicio do desenvolvimento frm de login e classes java

---
+++ AvaliacaoWeb/src/java/br/edu/utfpr/connect/Conexao.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.connect;
+
+import java.sql.*;
+/**
+ *
+ * @author Aluno
+ */
+public class Conexao {
+    public static boolean checkUser(String email,String pass) 
+     {
+      boolean st =false;
+      try{
+
+	 //loading drivers for mysql
+         Class.forName("com.mysql.jdbc.Driver");
+
+ 	 //creating connection with the database 
+         Connection con=DriverManager.getConnection
+                        ("jdbc:mysql:/ /localhost:3306/test","root","studytonight");
+         PreparedStatement ps =con.prepareStatement
+                             ("select * from register where email=? and pass=?");
+         ps.setString(1, email);
+         ps.setString(2, pass);
+         ResultSet rs =ps.executeQuery();
+         st = rs.next();
+        
+      }catch(Exception e)
+      {
+          e.printStackTrace();
+      }
+         return st;                 
+  }   
+}
---
+++ AvaliacaoWeb/src/java/br/edu/utfpr/login/Login.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.login;
+
+import br.edu.utfpr.connect.Conexao;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class Login extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        
+        if(Conexao.checkUser(email, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
+    }  
+}
